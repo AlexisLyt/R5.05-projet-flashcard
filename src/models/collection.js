422317fd0ef1a3f08eff6f5a7a4d393f94fa0b1a
Maceo David <maceod23@gmail.com>
2025-12-11 15:00:13 +0100

    collection model

---
+++ src/models/collection.js
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const createCollectionSchema = z.object({
+    title: z.string().min(1).max(300),
+    description: z.string().min(1).max(300).nullish(),
+    visibility: z.enum(["public", "private"])
+});
+
+export const collectionIdSchema = z.object({
+    id: z.uuid()
+});
